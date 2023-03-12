df2cb57dc656488595554b0ead339030fbae2e80
Dummy-da-Silva-Junior <127640478+Dummy-da-Silva-Junior@users.noreply.github.com>
2023-03-11 23:34:55 -0300

    Create script.py

---
+++ script.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+def func(e):
+  return len(e)
+ 
+ x=int(input())
+ vet=[]
+
+for i in range(0,x):
+  aux=input()"
+  x.append(aux)"
+
+x.sort(key=func)
+
+for s in x:
+  print(s)
